b9054623e98e05dfcdcc1e8ab83221a3f79a4026
Lokesh Singh Rajwar <lokeshsingh@codez.in>
2022-07-26 16:20:41 +0530

    Custom_User

---
+++ django_custom_user_model-master/account/admin.py
@@ -0,0 +1,37 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from .forms import UserChangeForm, UserCreationForm
+from .models import User,Book,Cart,Customer
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('email', 'date_of_birth', 'is_admin')
+    list_filter = ('is_admin',)
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth',)}),
+        ('Permissions', {'fields': ('is_admin',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2')}
+         ),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+    filter_horizontal = ()
+
+# Register your models here.
+
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
+admin.site.register(Book)
+admin.site.register(Cart)
+admin.site.register(Customer)
---
+++ django_custom_user_model-master/account/models.py
@@ -0,0 +1,83 @@
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser, User)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name='email',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+# Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=200,null=True)
+    Author=models.CharField(max_length=200,null=True)
+    Price=models.IntegerField()
+    Edition=models.IntegerField()
+
+    def __str__(self):
+        return str(self.title)
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name=models.CharField(max_length=200,null=True)
+    phone=models.CharField(max_length=200,null=True)
+    email=models.CharField(max_length=200,null=True)
+    date_created=models.DateTimeField(auto_now_add=True,null=True)
+    
+    def __str__(self):
+        return str(self.name)
+
+class Cart(models.Model): 
+    customer=models.OneToOneField(Customer, null=True, on_delete=models.CASCADE)  
+    books=models.ManyToManyField(Book)    
+
+    def __str__(self):
+        return str(self.customer)
